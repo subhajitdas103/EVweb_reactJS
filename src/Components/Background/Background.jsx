65d42aa6ea4d619f3fc9257913208dad4ace6381
subhajit das <subh.avalgate@gmail.com>
2024-10-25 12:20:59 +0530

    Removed video1.mp4 references

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,22 +1,19 @@
 import './Background.css';
-import video1 from '../../Assets/video1.mp4';
+
+import image4 from '../../Assets/image4.jpg';
 import image1 from '../../Assets/image1.png';
 import image2 from '../../Assets/image2.jfif';
 import image3 from '../../Assets/image3.png';
 
 const Background = ({ playStatus, heroCount }) => {
   if (playStatus) {
-    return (
-      <video className="background" autoPlay loop muted>
-        <source src={video1} type="video/mp4" />
-      </video>
-    );
+    return <img src={image4} className="background" alt="background" />;
   } else if (heroCount === 0) {
-    return <img src={image1} className="background" alt="" />;
+    return <img src={image1} className="background" alt="background" />;
   } else if (heroCount === 1) {
-    return <img src={image2} className="background" alt="" />;
+    return <img src={image2} className="background" alt="background" />;
   } else if (heroCount === 2) {
-    return <img src={image3} className="background" alt="" />;
+    return <img src={image3} className="background" alt="background" />;
   }
 
   // Return null or a default fallback if none of the conditions are met
